848e569f50f99f04023220ac99e6d6dd6f6d34a9
shadowfox3735 <normanpan180@gmail.com>
2018-10-29 16:28:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,6 +12,14 @@ class TicTacToe
   [0, 4, 8],
   [6, 4, 2]
 ]
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts '-----------'
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts '-----------'
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
 def play
   turn(board) until over?(board)
   if won?(board)
@@ -19,14 +27,6 @@ def play
   elsif draw?(board)
     puts "Cat's Game!"
   end
-end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts '-----------'
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts '-----------'
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
